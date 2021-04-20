05b6073c2aa170d448604bac76c4af8de7ad403a
Mustafa BÜKÜLMEZ <mustafabukulmez3446@gmail.com>
2021-04-20 13:29:14 +0300

    using to template

--- cpp_simple_calculator/main.cpp
+++ cpp_simple_calculator/main.cpp
@@ -4,42 +4,34 @@
 using namespace std;
 
 
-
-double final = 0;
-int calculate(char op, int num1, int num2)
-{
-	switch (op)
-	{
-	case '+':
-		final = num1 + num2;
-		break;
-	case '*':
-		final = num1 * num2;
-		break;
-	case '-':
-		final = num1 - num2;
-		break;
-	case '/':
-		final = num1 / num2;
-		break;
-
-	default:
-		cout << "wrong operator";
-		break;
-	}
-
-	cout << "Final is: " << final << endl;
-	cout << "--------------------" << endl;
-	return final;
-}
-
 void start()
 {
 	cout << "Welcome to Calculator v1..." << endl;
 	cout << "www.mustafabukulmez.com" << endl;
-
 	cout << "--------- Use -!- while operator input for Exit  -------" << endl;
 	cout << "--------------------------------------" << endl;
+}
+
+
+
+double final = 0;
+char op;
+
+
+template <typename T> T Calculate(T num1, T num2)
+{
+	switch (op)
+	{
+	case '+':final = num1 + num2; break;
+	case '*':final = num1 * num2; break;
+	case '-':final = num1 - num2; break;
+	case '/':final = num1 / num2; break;
+
+	default:cout << "wrong operator"; break;
+	}
+	cout << "Final is: " << final << endl;
+	cout << "--------------------" << endl;
+	return final;
 }
 
 
@@ -47,7 +39,6 @@ int main()
 {
 	start();
 	double num2 = 0, num1 = 0;
-	char op;
 
 	cout << "input a number" << endl;
 	cin >> num1;
@@ -55,19 +46,15 @@ int main()
 Next:
 	cout << "input an operator" << endl;
 	cin >> op;
-
 	if (op == '!')exit(0);
+
 
 	cout << "input a number" << endl;
 	cin >> num2;
 
-	num1 = calculate(op, num1, num2);
+	num1 = Calculate(num1, num2);
+
 	goto Next;
-
-
-
-
-
 
 
 }
